51f0834b2e3e894dcad183c6b451b43d1f07e1f1
Victor Villarreal <mefhigoseth@gmail.com>
2020-03-23 14:54:48 -0300

    fix(pwa): Mejorado el manifest.json

--- app/src/components/Turnos.js
+++ app/src/components/Turnos.js
@@ -1,13 +1,6 @@
 // Importamos librerias instaladas
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-//import Card from '@material-ui/core/Card';
-//import CardActions from '@material-ui/core/CardActions';
-//import CardActionArea from '@material-ui/core/CardActionArea';
-//import CardContent from '@material-ui/core/CardContent';
-//import CardMedia from '@material-ui/core/CardMedia';
-//import Button from '@material-ui/core/Button';
-//import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 // Importamos componentes locales
@@ -57,14 +50,14 @@ export default class Turnos extends React.Component {
 
     //const classes = this.useStyles();
     const farmacias = this.state.farmacias;
-    console.log('json: ', farmacias);
+    //console.log('json: ', farmacias);
 
     return (
       <Grid container justify='center' spacing={2}>
 
         {
           //JSON.stringify(farmacias['rafael-calzada'])
-          farmacias.map( farmacia => <FarmaciaCard farmacia={farmacia} /> )
+          farmacias.map( farmacia => <FarmaciaCard farmacia={farmacia} key={farmacia.id}/> )
         }
 
       </Grid>
