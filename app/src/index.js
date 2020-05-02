d97bf4efa10a5ac4f6de5f9f069378b6c951aa80
Victor Villarreal <mefhigoseth@gmail.com>
2020-05-02 04:23:21 -0300

    feat(app): Agregamos aviso de nueva version cuando el ServiceWorker esta activo

--- app/src/index.js
+++ app/src/index.js
@@ -10,4 +10,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: registration => {
+    alert('Nueva version disponible!  Listo para actualizar?');
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  }
+});
