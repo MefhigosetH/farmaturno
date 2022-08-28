1842939e1824157cda7705f167ddbc469d934cd8
Victor Villarreal <mefhigoseth@gmail.com>
2022-08-28 01:31:42 -0300

    fix(serviceworker): Deshabilitamos el ServiceWorker

--- app/src/index.js
+++ app/src/index.js
@@ -4,13 +4,14 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import App from './components/App';
 
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+/*
 serviceWorker.register({
   onUpdate: registration => {
     alert('Nueva version disponible!  Listo para actualizar?');
@@ -20,3 +21,4 @@ serviceWorker.register({
     window.location.reload();
   }
 });
+*/
