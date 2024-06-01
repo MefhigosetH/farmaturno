1168da250e75c6bc598c45e75836e8d1b8ed4ae7
Victor Villarreal <mefhigoseth@gmail.com>
2024-05-31 23:12:59 -0300

    Update Turnos.js
    Actualizamos la descripción al 2024. Gracias al usuario Dai de cafecito.app por avisar.

--- app/src/components/Turnos.js
+++ app/src/components/Turnos.js
@@ -107,7 +107,7 @@ class Turnos extends React.Component {
             <Paper sx={{ m: 2, px: 2, py: 0.5}}>
               <h3>FARMACIAS DE TURNO EN ALMIRANTE BROWN</h3>
               <ul>
-                <li>Ya están disponibles los turnos 2023, para Rafael Calzada y Claypole !</li>
+                <li>Ya están disponibles los turnos 2024, para Rafael Calzada y Claypole !</li>
                 <li>Cada turno comienza a las 08:30 Hs del día indicado y termina a las 08:30 del día siguiente.</li>
                 <li>Todas las distancias se calculan, por el momento, desde la Estaci&oacute;n de Rafael Calzada.</li>
               </ul>
